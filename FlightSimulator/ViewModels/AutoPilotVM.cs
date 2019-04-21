7e18dc914368347c7f57ee555e9e89f787761de6
Eyal <eyalcohen308@gmail.com>
2019-04-21 17:56:55 +0300

    added joystick and TextBox

---
+++ FlightSimulator/ViewModels/AutoPilotVM.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FlightSimulator.Model;
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotVM : BaseNotify
+    {
+        private string color;
+        private string text;
+        private ICommand _okCommand;
+        private ICommand _clearCommand;
+        private bool startedWrite;
+        /**
+         * CTOR:
+         * */
+        public AutoPilotVM()
+        {
+            color = "White";
+            text = "";
+            startedWrite = false;
+            _okCommand = null;
+            _clearCommand = null;
+        }
+        public string TextProperty
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                startedWrite = true;
+                text = value;
+                NotifyPropertyChanged("TextProperty");
+                NotifyPropertyChanged("Color");
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                color = startedWrite ? "Pink" : "White";
+                return color;
+            }
+        }
+        public ICommand clearCommand
+        {
+            get
+            {
+                return _clearCommand ?? (_clearCommand = new CommandHandler(() => FlushBox()));
+            }
+        }
+        public ICommand okCommand
+        {
+            get
+            {
+                return _okCommand ?? (_okCommand = new CommandHandler(() => SendInstructions()));
+            }
+        }
+
+        private void FlushBox()
+        {
+            startedWrite = false;
+            text = "";
+            NotifyPropertyChanged("TextProperty");
+            NotifyPropertyChanged("Color");
+        }
+        private void SendInstructions()
+        {
+            //...
+            startedWrite = false;
+            //text = "";
+            //NotifyPropertyChanged("TextProperty");
+            NotifyPropertyChanged("Color");
+        }
+    }
+}
