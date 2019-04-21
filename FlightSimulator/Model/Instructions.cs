c99a7edfb338a1a6c0c78c25475f878cf47de7af
Eyal <eyalcohen308@gmail.com>
2019-04-21 19:55:11 +0300

    added 1 model

---
+++ FlightSimulator/Model/Instructions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using FlightSimulator.Model.Interface;
+using System.Net.Sockets;
+namespace FlightSimulator.Model
+{
+    class Instructions
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private readonly object mut;
+        private static Instructions instructions=null;
+        private Dictionary<string, string> commandsToPath;
+        
+        /**
+         * Private CTOR - SingleTon.
+         **/
+         private Instructions()
+        {
+            client = null;
+            stream = null;
+            commandsToPath = new Dictionary<string, string>();
+            mut = new object();
+            InitPathMapValues();
+        }
+        public static Instructions getInstance
+        {
+            get
+            {
+                // if exists return instance, else create it and return.
+                return null == instructions ? new Instructions() : instructions;
+            }
+        }
+        public void ManualSendCommand(string command,double val)
+        {
+            if (client == null)
+            {
+                return;
+            }
+            string line = "set " + commandsToPath[command] + " ";
+            line += val.ToString("N5");
+            lock (mut)
+            {
+                byte[] buffer = System.Text.Encoding.ASCII.GetBytes(line.ToString());
+                stream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("command: " + line + " sent");
+            }
+        }
+        private void InitPathMapValues()
+        {
+            commandsToPath.Add("rudder", "/controls/flight/rudder");
+            commandsToPath.Add("throttle", "/controls/engines/current-engine/throttle");
+            commandsToPath.Add("aileron", "/controls/flight/aileron");
+            commandsToPath.Add("elevator", "/controls/flight/elevator");
+        }
+    }
+}
---
+++ FlightSimulator/ViewModels/ManualVM.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    class ManualVM : BaseNotify
+    {
+        private double _aileron;
+        private double _throttle;
+        private double _rudder;
+        private double _elevator;
+        public ManualVM()
+        {
+            _aileron = 0;
+            _elevator = 0;
+            _rudder = 0;
+            _throttle = 0;
+        }
+        public double ChangeRudder
+        {
+            get
+            {
+                return _rudder;
+            }
+            set
+            {
+                _rudder = value;
+                NotifyPropertyChanged("ChangeRudder");
+                // Send to property changed to commands and from there to server. 
+            }
+
+        }
+        public double ChangeThrottle
+        {
+            get
+            {
+                return _throttle;
+            }
+            set
+            {
+                _throttle = value;
+                NotifyPropertyChanged("ChangeThrottle");
+                // Send to property changed to commands and from there to server. 
+            }
+        }
+        public double ChangeAileron
+        {
+            get
+            {
+                return _aileron;
+            }
+            set
+            {
+                _aileron = value;
+                NotifyPropertyChanged("ChangeAileron");
+                // Send to property changed to commands and from there to server. 
+            }
+        }
+        public double ChangeElevator
+        {
+            get
+            {
+                return _elevator;
+            }
+            set
+            {
+                _elevator = value;
+                NotifyPropertyChanged("ChangeElevator");
+                // Send to property changed to commands and from there to server. 
+            }
+        }
+    }
+    }
